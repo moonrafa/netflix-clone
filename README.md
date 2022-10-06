f8292c9ce875f61739e7ce2d3c2d8b60081d81aa
Rafaelly Rodrigues <rrafasrodrigues@gmail.com>
2022-10-05 22:15:57 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Netflix Clone with User and Payment Authentication
+
+## Tools
+
+- Reactjs
+- CSS
+- Stripe Checkout & Webhooks
+- Redux
+- Firestore
+- Google Auth
+- TMDB API
+
+## Demo
+
+## Author
+
+Rafaelly Rodrigues
+
+- @moonrafa
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore'
+import 'firebase/compat/auth'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD9HCheK5Z9y9PDhTMGhjEyhrzqoLm8A0A',
+  authDomain: 'netflix-clone-moonrafa.firebaseapp.com',
+  projectId: 'netflix-clone-moonrafa',
+  storageBucket: 'netflix-clone-moonrafa.appspot.com',
+  messagingSenderId: '950328069614',
+  appId: '1:950328069614:web:3ce1076e65360c6b5d92ff'
+}
+firebase.initializeApp(firebaseConfig)
+
+const db = firebase.firestore()
+const auth = firebase.auth()
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+const provider = new firebase.auth.GoogleAuthProvider()
+
+export { db, auth, timestamp, provider }
