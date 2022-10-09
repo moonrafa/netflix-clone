d5d4cc441119def32d803ec07f924a6369ad917a
Rafaelly Rodrigues <rrafasrodrigues@gmail.com>
2022-10-09 14:58:41 -0300

    + row of movies

--- src/components/Homescreen.js
+++ src/components/Homescreen.js
@@ -3,14 +3,26 @@ import React from 'react'
 import Nav from './Nav'
 import Banner from './Banner'
 import Row from './Row'
+import requests from '../Request'
 
 function Homescreen() {
   return (
     <div className="homescreen">
       <Nav />
       <Banner />
-      {/*
-        <Row /> */}
+
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isTheRowLarge
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
     </div>
   )
 }
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Row.css'
+import axios from '../axios'
 
-function Row() {
+function Row({ title, fetchUrl, isTheRowLarge = false }) {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl)
+      setMovies(request.data.results)
+      return request
+    }
+    fetchData()
+  }, [fetchUrl])
+
   return (
     <div className="row">
-      <h1>Row</h1>
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map(
+          movie =>
+            ((isTheRowLarge && movie.poster_path) ||
+              (!isTheRowLarge && movie.backdrop_path)) && (
+              <img
+                className={`row__poster ${isTheRowLarge && 'row__posterLarge'}`}
+                key={movie.id}
+                src={`https://image.tmdb.org/t/p/original/${
+                  isTheRowLarge ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt={movie.name}
+              />
+            )
+        )}
+      </div>
     </div>
   )
 }
